11b0b748898cb9dd4141cea4e11b38797bb4d6d0
DaviBisewski <davimbisewski@gmail.com>
2025-06-01 11:28:57 -0300

    feat: Adição página direcionamento de login e cadastro #10

---
+++ src/components/AuthButton.vue
@@ -0,0 +1,35 @@
+<template>
+  <button :class="[filled ? 'filled' : 'outlined']">
+    <slot />
+  </button>
+</template>
+
+<script setup>
+defineProps({
+  filled: Boolean
+})
+</script>
+
+<style scoped>
+.filled {
+  width: 100%;
+  padding: 1rem 0;
+  border-radius: 0.375rem;
+  font-size: 0.875rem;
+  font-weight: 500;
+  background-color: #1b3d1f; /* verde escuro */
+  color: white;
+  border: none;
+}
+
+.outlined {
+  width: 100%;
+  padding: 1rem 0;
+  border-radius: 0.375rem;
+  font-size: 0.875rem;
+  font-weight: 500;
+  background-color: white;
+  color: #1b3d1f;
+  border: 1px solid #1b3d1f;
+}
+</style>
--- src/views/PerfilView.vue
+++ src/views/PerfilView.vue
@@ -1,1 +1,47 @@
-<template>a</template>
+<script setup>
+import AuthButton from '../components/AuthButton.vue'
+import BackButton from '../components/BackButton.vue'
+</script>
+<template>
+  <div class="login-page">
+    <BackButton />
+
+    <div class="login-card">
+      <AuthButton filled>Fazer Login</AuthButton>
+      <div class="spacer" />
+      <AuthButton>Criar Conta</AuthButton>
+
+      <img src="../assets/img/logo.png" alt="Logo" class="logo" />
+
+    </div>
+  </div>
+  <RouterView />
+</template>
+
+<style scoped>
+.login-page {
+  min-height: 85dvh;
+  background-color: #e5e5e5; /* cinza claro */
+  display: flex;
+  flex-direction: column;
+}
+
+.login-card {
+  margin-top: auto;
+  background-color: white;
+  border-top-left-radius: 1.5rem;
+  border-top-right-radius: 1.5rem;
+  padding: 1.5rem;
+}
+
+.spacer {
+  height: 0.9rem;
+}
+
+.logo {
+  height: 80px; /* 48px */
+  width: 130px; /* 48px */
+  margin: 1rem auto 0;
+  display: block;
+}
+</style>
