5bd7742f40ae9c29edc66dadb6a6460f02fc20d1
DaviBisewski <davimbisewski@gmail.com>
2025-05-31 22:10:22 -0300

    feat: Ajuste nos icons #2

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -3,22 +3,22 @@
 
 <template>
   <header class="header">
-    <!-- Botão da logo (agora à esquerda) -->
+    <route-link to="/" >
     <button class="logo-button">
       <img src="@/assets/img/logo.png" alt="Logo Galinha" class="logo-img" />
     </button>
+    </route-link>
 
-    <!-- Endereço centralizado -->
     <div class="address-selector">
-      <span class="address-text">{{ Rua }}Rua</span>
+      <span class="address-text">{{ Rua }}Rua Ascurra, 125</span>
       <svg xmlns="http://www.w3.org/2000/svg" class="arrow-icon" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
       </svg>
     </div>
 
-    <!-- Botão do celular (agora à direita) --> 
+  
     <button class="phone-button">
-      <img src="@/assets/img/cellphone.png" alt="Telefone" class="phone-icon" />
+      <img src="@/assets/img/phone-call.png" alt="Telefone" class="phone-icon" />
     </button>
   </header>
 </template>
@@ -37,7 +37,7 @@
   position: relative;
 }
 
-/* Centraliza o seletor de endereço */
+
 .address-selector {
   position: absolute;
   left: 50%;
@@ -49,7 +49,7 @@
 }
 
 .address-text {
-  font-size: 1.125rem;
+  font-size: 0.8rem;
 }
 
 .arrow-icon {
@@ -66,13 +66,13 @@
 }
 
 .logo-img {
-  width: 90px;
-  height: 60px;
+  width: 80px;
+  height: 50px;
 }
 
 .phone-icon {
-  width: 5vh;
-  height: 5vh;
-  right: 2rem;
+  width: 30px;
+  height: 30px;
+  margin-right: 1.5rem;
 }
 </style>
