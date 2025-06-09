8a8205a2ca9a469fc5bef6f93afeea1b403c8c60
DaviBisewski <davimbisewski@gmail.com>
2025-06-09 20:13:12 -0300

    feat: Criação view form endereço #14

--- src/views/EnderecoFormView.vue
+++ src/views/EnderecoFormView.vue
@@ -1,47 +1,34 @@
 <template>
   <div class="form-page">
-    <!-- Botão Voltar -->
+    
     <div class="back-button-wrapper">
       <BackButton />
     </div>
 
-    <!-- Campos -->
+    
     <form class="form">
-      <InputForm label="Digite seu Nome Completo" v-model="form.nome" />
-      <InputForm label="Digite seu Telefone" v-model="form.telefone" />
-      <InputForm label="Digite seu Endereço" v-model="form.endereco" />
-      <InputForm label="Número" v-model="form.numero" />
-      <InputForm label="Complemento (opcional)" v-model="form.complemento" />
+      <InputForm label="Bairro" />
+      <InputForm label="Rua" />
+      <InputForm label="Cep" />
+      <InputForm label="Número" />
+      <InputForm label="Complemento"  />
     </form>
 
-    <!-- Botão Continuar -->
-    <ContinueButton @click="enviarFormulario" />
+
+    <ContinueButton  />
   </div>
 </template>
 
 <script setup>
-import { reactive } from 'vue'
 import BackButton from '@/components/buttons/BackButton.vue'
 import InputForm from '@/components/forms/InputForm.vue'
 import ContinueButton from '@/components/buttons/ContinueButton.vue'
 
-const form = reactive({
-  nome: '',
-  telefone: '',
-  endereco: '',
-  numero: '',
-  complemento: '',
-})
-
-function enviarFormulario() {
-  console.log('Dados enviados:', form)
-  // Aqui você pode fazer a navegação ou enviar dados para a API
-}
 </script>
 
 <style scoped>
 .form-page {
-  padding: 24px 16px 80px; /* espaço extra no final por causa do botão fixo */
+  padding: 24px 16px 80px;
   min-height: 100vh;
   box-sizing: border-box;
 }
@@ -56,5 +43,6 @@ function enviarFormulario() {
   flex-direction: column;
   gap: 16px;
   margin-top: 24px;
+  font-weight: 550;
 }
 </style>
