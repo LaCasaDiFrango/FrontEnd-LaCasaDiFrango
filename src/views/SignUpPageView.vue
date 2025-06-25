7a21865ed471c360254493e333d63c3a914312bc
HenriqueChepli <henriquechepli@gmail.com>
2025-06-25 14:22:24 -0300

    feat: Começando do zero a aplicação do Passage Id #24

--- src/views/SignUpPageView.vue
+++ src/views/SignUpPageView.vue
@@ -1,23 +1,20 @@
 <template>
   <div class="register-page">
     <BackButton />
-
     <!-- Componente de login Passage -->
     <passage-auth app-id="WEG5lzsWo0bbZlaYwtVYYb6Q"></passage-auth>
-
-    <!-- Formulário extra (só aparece após login via Passage) -->
-      <ContinueButton type="submit" />
   </div>
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue'
+import { onMounted } from 'vue'
 import '@passageidentity/passage-elements/passage-auth'
-import BackButton from '@/components/buttons/BackButton.vue'
 import InputForm from '@/components/forms/InputForm.vue'
-import ContinueButton from '@/components/buttons/ContinueButton.vue'
+import BackButton from '@/components/buttons/BackButton.vue'
 import api from '@/api/api'
+
 </script>
+
 
 <style scoped>
 .register-page {
@@ -25,11 +22,5 @@ import api from '@/api/api'
   display: flex;
   flex-direction: column;
   background-color: white;
-}
-
-.form-content {
-  padding: 1.5rem;
-  flex-grow: 1;
-  font-weight: 550;
 }
 </style>
