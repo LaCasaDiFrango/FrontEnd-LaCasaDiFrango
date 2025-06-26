19df58d1deafa9434255392a8dfc173f138ac669
HenriqueChepli <henriquechepli@gmail.com>
2025-06-26 13:46:05 -0300

    feat: integrate Passage login with auth store; token saving not fully working yet #24

--- src/views/SignUpPageView.vue
+++ src/views/SignUpPageView.vue
@@ -1,18 +1,58 @@
 <template>
   <div class="register-page">
     <BackButton />
-    <!-- Componente de login Passage -->
-    <passage-auth app-id="WEG5lzsWo0bbZlaYwtVYYb6Q"></passage-auth>
+  <passage-auth app-id="WEG5lzsWo0bbZlaYwtVYYb6Q"></passage-auth>
+<!--
+<form class="form-content">
+  <InputForm
+    v-model="name"
+    label="Digite seu Nome Completo"
+    placeholder="Nome completo"
+  />
+
+  <InputForm
+    v-model="email"
+    label="Digite seu Email"
+    placeholder="exemplo@gmail.com"
+    type="email"
+  />
+  <InputForm
+    v-model="password"
+    label="Digite sua Senha"
+    placeholder="Senha"
+    type="password"
+  />
+</form>
+-->
   </div>
 </template>
 
 <script setup>
-import { onMounted } from 'vue'
-import '@passageidentity/passage-elements/passage-auth'
-import InputForm from '@/components/forms/InputForm.vue'
+import { ref } from 'vue'
 import BackButton from '@/components/buttons/BackButton.vue'
-import api from '@/api/api'
+import InputForm from '@/components/forms/InputForm.vue'
+import '@passageidentity/passage-elements/passage-auth'
 
+// const name = ref('')
+// const email = ref('')
+// const password = ref('')
+
+// const handleSubmit = async () => {
+//   try {
+//     const payload = {
+//       name: name.value,
+//       email: email.value,
+//       password: password.value
+//     }
+
+//     const response = await api.post('/usuarios/', payload)
+//     console.log('Usuário cadastrado com sucesso:', response.data)
+//     alert('Cadastro realizado!')
+//   } catch (error) {
+//     console.error('Erro ao cadastrar usuário:', error.response?.data || error.message)
+//     alert('Erro ao cadastrar usuário.')
+//   }
+// }
 </script>
 
 
@@ -22,5 +62,22 @@ import api from '@/api/api'
   display: flex;
   flex-direction: column;
   background-color: white;
+}
+
+.form-content {
+  padding: 1.5rem;
+  flex-grow: 1;
+  font-weight: 550;
+}
+
+.button-footer {
+  position: sticky;
+  bottom: 0;
+  background-color: #1b3d1f;
+  padding: 1rem;
+}
+
+.button-footer button {
+  width: 100%;
 }
 </style>
