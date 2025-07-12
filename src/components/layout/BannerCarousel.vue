fba4357ba21a5a02f540e41992780b00b2d2b7b6
DaviBisewski <davimbisewski@gmail.com>
2025-07-11 21:31:42 -0300

    feat: Implementação carrocel de banners #30

---
+++ src/components/layout/BannerCarousel.vue
@@ -0,0 +1,39 @@
+<!-- src/components/BannerCarousel.vue -->
+<template>
+  <Splide :options="options" aria-label="Banner Carousel">
+    <SplideSlide v-for="(banner, index) in banners" :key="index">
+      <img :src="banner" class="banner-image" />
+    </SplideSlide>
+  </Splide>
+</template>
+
+<script setup>
+import '@splidejs/splide/dist/css/splide.min.css'
+import { Splide, SplideSlide } from '@splidejs/vue-splide'
+
+const banners = [
+  'src/assets/img/',
+  'src/assets/img/',
+  'src/assets/img/',
+]
+
+const options = {
+  type: 'loop',
+  autoplay: true,
+  interval: 4000,
+  pauseOnHover: true,
+  arrows: false,
+  pagination: true,
+  gap: '1rem',
+  heightRatio: 0.4,
+}
+</script>
+
+<style scoped>
+.banner-image {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  border-radius: 12px;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,13 @@
 <script setup>
 import CategoryNav from '@/components/layout/CategoryNav.vue'
+import BannerCarousel from '@/components/layout/BannerCarousel.vue';
 </script>
 
 <template>
   <div class="home-page">
 
     <CategoryNav />
+    <BannerCarousel/>
    
   </div>
 </template>
