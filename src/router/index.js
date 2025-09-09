7f332e8c982a251a74c8085b5c2ac6794f313f28
DaviBisewski <davimbisewski@gmail.com>
2025-09-08 21:51:47 -0300

    feat: Criação pagina Historia e button router #79

--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,7 @@ import HistoricoPedidosView from '@/views/pedido/HistoricoPedidosView.vue'
 import DetalhesPedidoView from '@/views/pedido/DetalhesPedidoView.vue'
 import DetalhesPagamentoView from '@/views/pagamento/DetalhesPagamentoView.vue'
 import AtualizarUsuario from '@/views/usuario/AtualizarUsuario.vue'
+import HistoriaView from '@/views/HistoriaView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -108,6 +109,11 @@ const router = createRouter({
       path: '/home/pedidos/detalhes-pagamento',
       name: 'detalhes-pagamento',
       component: DetalhesPagamentoView,
+    },
+       {
+      path: '/historia',
+      name: 'historia',
+      component: HistoriaView,
     }
   ],
 })
