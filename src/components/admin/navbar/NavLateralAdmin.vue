df0f959c6ba75b6610aa8c1baebc6e5ed4b1a6f1
DaviBisewski <davimbisewski@gmail.com>
2025-11-29 18:06:15 -0300

    feat: simplificar estrutura do componente NavLateralAdmin e ajustar posicionamento do logo

--- src/components/admin/navbar/NavLateralAdmin.vue
+++ src/components/admin/navbar/NavLateralAdmin.vue
@@ -1,31 +1,3 @@
-<template>
-  <div v-if="!store.isMobile" class="flex">
-    <aside
-      class="fixed top-0 left-0 h-screen w-64 bg-gray-100 p-5 flex flex-col gap-8 z-50"
-    >
-      <NavbarAdminUser />
-      <NavbarAdminMenu />
-
-      <div class="flex justify-center items-center w-full mt-auto">
-        <img
-          src="@/assets/img/logo.png"
-          alt="Logo"
-          class="w-32 h-auto mb-5"
-        />
-      </div>
-    </aside>
-
-    <!-- Conteúdo principal deslocado -->
-    <div class="ml-64 w-full">
-      <slot />
-    </div>
-  </div>
-
-  <div v-else>
-    <NavLateralResponsive />
-  </div>
-</template>
-
 <script setup>
 import { useNavBarAdminStore } from '@/stores/index'
 import { NavLateralResponsive, NavbarAdminUser, NavbarAdminMenu } from '@/components/index'
@@ -33,5 +5,24 @@ import { NavLateralResponsive, NavbarAdminUser, NavbarAdminMenu } from '@/compon
 const store = useNavBarAdminStore()
 store.initResizeListener()
 </script>
+
+<template>
+  <div v-if="!store.isMobile" class="flex">
+    <aside class="min-h-screen bg-gray-100 p-5 flex flex-col gap-8">
+      <NavbarAdminUser />
+      <NavbarAdminMenu />
+      <div class="flex justify-center items-center w-full">
+                    <img
+        src="@/assets/img/logo.png"
+        alt="Logo"
+        class="fixed bottom-5 w-32 h-auto"
+      />
+      </div>
+    </aside>
+  </div>
+  <div v-else>
+    <NavLateralResponsive />
+  </div>
+</template>
 
 <style scoped></style>
